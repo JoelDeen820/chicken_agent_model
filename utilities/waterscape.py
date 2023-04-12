9aca81917caa1aaeb248afd1dc4c75d683488b76
joeldeen820 <joeldeen820@gmail.com>
2023-04-12 15:38:37 -0400

    Got a version of the waterline enviroment set up

--- utilities/waterscape.py
+++ utilities/waterscape.py
@@ -4,8 +4,23 @@ from skimage.draw import disk
 from chicken_utils import CENTIMETERS_PER_PIXEL
 
 
-class WaterScape:
+def make_visible_locs(vision):
+    """Computes the kernel of visible cells.
 
+    vision: int distance
+    """
+
+    def make_array(d):
+        """Generates visible cells with increasing distance."""
+        a = np.array([[-d, 0], [d, 0], [0, -d], [0, d]])
+        np.random.shuffle(a)
+        return a
+
+    arrays = [make_array(d) for d in range(1, vision + 1)]
+    return np.vstack(arrays)
+
+
+class WaterScape:
     DISTANCE_BETWEEN_NIPPLES = 0.1  # in meters
     WATER_RADIUS = 0.01  # in meters
     DISTANCE_BETWEEN_LINES = 3.0  # in meters
@@ -14,8 +29,18 @@ class WaterScape:
     WATER_RADIUS_PIXELS = WATER_RADIUS / CENTIMETERS_PER_PIXEL
     PIXELS_BETWEEN_NIPPLES = DISTANCE_BETWEEN_NIPPLES / CENTIMETERS_PER_PIXEL
 
+    def __draw_water_radius(self, points: list[tuple[int, int]]) -> None:
+        """Draws the water radius around the given points."""
+        for x, y in points:
+            rr, cc = disk((x, y), self.WATER_RADIUS_PIXELS, shape=self.size)
+            self.waterline_array[rr, cc] = 1
 
-    def __generate_waterlines(self) -> None:
+    def __draw_points(self, points: list[tuple[int, int]]) -> None:
+        """Draws the given points."""
+        for x, y in points:
+            self.waterline_array[x, y] = 1
+
+    def __generate_waterlines(self, points=True) -> None:
         """Generate the waterlines."""
         waterline_points = []
         for i in range(self.NUM_LINES):
@@ -24,9 +49,10 @@ class WaterScape:
                 y = self.waterline_offset_y + i * self.PIXELS_BETWEEN_LINES
                 waterline_points.append((x, y))
 
-        for x, y in waterline_points:
-            rr, cc = disk((x, y), self.WATER_RADIUS_PIXELS, shape=self.size)
-            self.waterline_array[rr, cc] = 1
+        if points:
+            self.__draw_points(waterline_points)
+        else:
+            self.__draw_water_radius(waterline_points)
 
     def __init__(self, size: tuple) -> None:
         """Initialize the WaterScape object.
@@ -39,4 +65,15 @@ class WaterScape:
         self.waterline_offset_y = size[1] // 2 - self.PIXELS_BETWEEN_LINES // 2
         self.waterline_array = np.zeros(size, dtype=np.uint8)
         self.__generate_waterlines()
+        self.__occupancy_grid = np.zeros(size, dtype=np.uint8)
 
+    def get_waterline_vision(self, center: tuple[int, int], vision: int) -> np.array:
+        """ Find the visible waterline points for bird consumption.
+
+        center: tuple, (x, y) in pixels
+        vision: int, distance in pixels
+
+        returns: np.array, (x, y) in pixels, where the points are waterlines visible."""
+        visible_locations = make_visible_locs(vision) + center
+        visible_space = self.waterline_array[visible_locations[:, 0], visible_locations[:, 1]]
+        return visible_locations[np.argwhere(visible_space == 1)][:, 0, :]
